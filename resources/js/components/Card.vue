9aea210440a9134bd936d84fdf6e3ed0c6683459
Andrey Palamarchuk <a.palamarchuk@corp.101xp.com>
2019-08-12 15:22:40 +0300

    Implemented logic for resetting current filter to empty object before index reloading

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -20,11 +20,9 @@ export default {
     methods:{
         async reloadResources(){
             if(this.resourceName){
-                let filters_backup = _.cloneDeep(this.$store.getters[`${this.resourceName}/filters`]);
-                let filters_to_change = _.cloneDeep(filters_backup);
-                filters_to_change.push({});
-                await this.$store.commit(`${this.resourceName}/storeFilters`,filters_to_change);
-                await this.$store.commit(`${this.resourceName}/storeFilters`,filters_backup);
+		let currentFilters = _.cloneDeep(this.$store.state[`${this.resourceName}`]['filters']);
+                await this.$store.commit(`${this.resourceName}/storeFilters`, {});
+                await this.$store.commit(`${this.resourceName}/storeFilters`, currentFilters);
             }
         }
     }
